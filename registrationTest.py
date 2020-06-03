d9ca6f8dd188fb4d9d732825db5b7f3d31a391e8
beata-fur <beata.furgacz1@gmail.com>
2020-06-03 15:01:41 +0200

    Gotowy test rejestracji z błędnym imieniem

--- registrationTest.py
+++ registrationTest.py
@@ -9,13 +9,15 @@ Przypadek testowy:
 Błędne imię (obecnośc znaku '@')
 
 Kroki:
-1. Kliknij "Zarejestruj się"
-2. Kliknij "Zarejestruj się"
-3. Wprowadz błędne imię i prawidłowe nazwisko
-4. Podaj nazwę profilu //"796344620"
-6. Wpisz prawidłowy adres e-mail // "horicana-2123@yopmail.com"
-7. Wpisz prawidłowe hasło // "t3gdMWFAtZ"
-8. Kliknij Zarejestruj sie
+
+1. Zaakceptuj zgodę cookies
+2. Kliknij "Zarejestruj się".
+3. Kliknij "Zarejestruj się" w nowym oknie.
+4. Wprowadź błędne imię i prawidłowe nazwisko.
+5. Podaj nazwę profilu.
+6. Wpisz prawidłowy adres e-mail.
+7. Wpisz prawidłowe hasło.
+8. Kliknij "Rejestracja".
 
 Oczekiwany rezultat:
 1. Uzytkownik dostaje informację na czerwono "Proszę podaj swoje imię i nazwisko"
@@ -28,4 +30,54 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 
-class vintedRegistration(unitttest.TestCase):
+class vintedRegistration(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.get("https://www.vinted.pl/")
+
+    def testWrongName(self):
+        driver = self.driver
+        #1. Zaakceptuj zgodę na cookies
+        # acceptation_btn = WebDriverWait(driver, 30)\
+        # .until(EC.presence_of_element_located((By.XPATH, 'button[path="[0,1,2]"]')))
+        # sleep(20)
+        # acceptation_btn = driver.find_element_by_xpath('button[path="[0,1,2]"]')
+        # acceptation_btn.click()
+
+        frame = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//iframe[contains(@id, "sp_message_iframe_")]')))
+        driver.switch_to.frame(frame)
+        # print(driver.page_source)
+
+        driver.find_element(By.XPATH, '//button[@path="[0,1,2]"]').click()
+
+        driver.switch_to.default_content()
+        #2. Kliknij "Zarejestruj się".
+        driver.find_element(By.XPATH,'//div[@class="l-header__actions"]//span[contains(text(),"Zarejestruj")]').click()
+        #3. Kliknij "Zarejestruj się" w nowym oknie.
+        driver.find_element(By.XPATH,'//div[@class="u-flex-grow"]//span[contains(text(),"Zarejestruj")]').click()
+        #4. Wprowadź błędne imię i prawidłowe nazwisko.
+        name_field = driver.find_element_by_id('user_real_name')
+        name_field.send_keys("Beat@ Kowalska")
+        #5. Podaj nazwę profilu.
+        user_nick_field = driver.find_element_by_id('user_login')
+        user_nick_field.send_keys("bea796344620")
+        #6. Wpisz prawidłowy adres e-mail.
+        email_field = driver.find_element_by_id('user_email')
+        email_field.send_keys("horicana-2123@yopmail.com")
+        #7. Wpisz prawidłowe hasło.
+        password_field = driver.find_element_by_id('user_password')
+        password_field.send_keys("t3gdMWFAtZ")
+        #8. Kliknij "Rejestracja".
+        registration_btt = driver.find_element_by_xpath('//div[@class="c-cell__body"]//span[contains(text(),"Rejestracja")]')
+        registration_btt.click()
+
+        sleep(10)
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
